6bdc7d73ca480fbfba31dffb970533ee26030bd8
Pavel Novikov <ya@pnovik.ru>
2024-04-04 21:54:51 +0300

    correction test.py

--- tests/test_HW_1.py
+++ tests/test_HW_1.py
@@ -7,14 +7,15 @@ from utils.HW1 import Category, Product
 
 @pytest.fixture()
 def Category_phone():
-    prod = ("iphone 11", "iphone 13", "iphone 12")
-    return Category('Сотовые телефоны', "Смартфоны", prod)
+    products = [Product('Iphone11', "new iphone", 28000, 3), Product('Iphone12', "new iphone", 38000, 4), Product('Iphone13', "new iphone", 58000, 2)]
+    return Category('Сотовые телефоны', "Смартфоны", products)
 
 def test_init(Category_phone):
     assert Category_phone.name == 'Сотовые телефоны'
     assert Category_phone.description == 'Смартфоны'
-    assert Category_phone.products[0] == 'iphone 11'
     assert Category_phone.product_count == 3
+    assert Category_phone.category_count == 1
+    assert len(Category_phone.products) == 3
     assert Category_phone.category_count == 1
 
 
@@ -23,7 +24,7 @@ def Product_phone():
     return Product('Iphone12', "new iphone", 58000, 2)
 
 
-def Product_init(Product_phone):
+def test_Product_init(Product_phone):
     assert Product_phone.name == 'Iphone12'
     assert Product_phone.description == 'new iphone'
     assert Product_phone.price == 58000
